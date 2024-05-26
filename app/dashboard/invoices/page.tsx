be6f3e396ec2cbbdbaed60c0c8367b8ffd345b70
ShababUlHassan <shababulhassan910@gmail.com>
2024-05-26 08:51:32 +0500

    initial commit

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,6 @@
+export default function Page(){
+    return <>
+            <h2>This is New invoice page</h2>
+            <p>Here you display the info regarding invoices</p>
+            </>
+}
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,4 @@
+
+export default function Page(){
+    return <p>New Dashboard Page</p>
+}
